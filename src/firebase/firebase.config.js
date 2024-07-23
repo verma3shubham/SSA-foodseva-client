68fbac82d1b7d47a2373f57bd8a5615feedc4ae7
shubham verma <shubhamverma9231@gmail.com>
2024-07-23 19:55:38 +0530

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaoKE7WpDsHi7OWqOmRcY5m7-U8ERyuvs",
+  authDomain: "ssa-food-seva.firebaseapp.com",
+  projectId: "ssa-food-seva",
+  storageBucket: "ssa-food-seva.appspot.com",
+  messagingSenderId: "946781141256",
+  appId: "1:439144801389:web:81b1e7f7f96334f2b58b64",
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
